89abe2d55e528d06f803be65a0f9156b88a91ec5
Tushar Naik <tushar.knaik@gmail.com>
2024-10-23 00:50:45 +0530

    removed unused imports

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "../components/ui/card";
 import {Badge} from "../components/ui/badge";
 import {format} from "date-fns";
 
